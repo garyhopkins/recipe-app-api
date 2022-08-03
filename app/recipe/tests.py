16c187e300281c652f2fcff85b53c571d4efb64e
Gary Hopkins <gary@verbidio.com>
2022-08-03 15:32:01 -0500

    added detailed recipe view

--- app/recipe/tests.py
+++ app/recipe/tests.py
@@ -10,9 +10,17 @@ from rest_framework.test import APIClient
 
 from core.models import Recipe
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import (
+    RecipeSerializer,
+    RecipeDetailSerializer,
+)
 
 RECIPES_URL = reverse("recipe:recipe-list")
+
+
+def detail_url(recipe_id):
+    """Create and return a recipe detail URL"""
+    return reverse("recipe:recipe-detail", args=[recipe_id])
 
 
 def create_recipe(user, **params):
@@ -80,4 +88,25 @@ class PrivateRecipeApiTests(TestCase):
         recipes = Recipe.objects.filter(user=self.user)
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_description_included_in_response(self):
+        """Test description field included in response data"""
+        recipe = create_recipe(user=self.user)
+        url = detail_url(recipe.id)
+
+        response = self.client.get(url)
+        serializer = RecipeDetailSerializer(recipe)
+
+        self.assertIn("description", response.data, response.data)
+
+    def test_get_recipe_detail(self):
+        """Test get recipe detail"""
+        recipe = create_recipe(user=self.user)
+        url = detail_url(recipe.id)
+
+        response = self.client.get(url)
+        serializer = RecipeDetailSerializer(recipe)
+        print(response.data)
+        print(serializer.data)
         self.assertEqual(response.data, serializer.data)
